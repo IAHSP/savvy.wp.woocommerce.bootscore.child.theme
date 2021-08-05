dd4162815dcdfa6123dda38375192e4f309bd1e8
gabe <djgtraxx@gmail.com>
2021-08-05 21:35:30 +0000

    changed my-account (not logged in) form to open side bar to use that login instead.  also changed the lost-password WP page to redirect to the iahsp one.

--- woocommerce/myaccount/form-login.php
+++ woocommerce/myaccount/form-login.php
@@ -19,59 +19,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-do_action( 'woocommerce_before_customer_login_form' ); ?>
+//do_action( 'woocommerce_before_customer_login_form' ); ?>
 
 <?php if ( 'yes' === get_option( 'woocommerce_enable_myaccount_registration' ) ) : ?>
 
 <div class="row" id="customer_login">
 
     <div class="col-md-6">
-
-        <?php endif; ?>
-
-
-
-
-        <h2><?php esc_html_e( 'Login', 'woocommerce' ); ?></h2>
-
-        <div class="card mt-3 mb-4">
-
-            <form class="card-body" method="post">
-
-                <?php do_action( 'woocommerce_login_form_start' ); ?>
-
-                <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                    <label for="username"><?php esc_html_e( 'Username or email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-                    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="username" id="username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
-                </p>
-                <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                    <label for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-                    <input class="woocommerce-Input woocommerce-Input--text input-text form-control" type="password" name="password" id="password" autocomplete="current-password" />
-                </p>
-
-                <?php do_action( 'woocommerce_login_form' ); ?>
-
-                <p class="form-check mb-3">
-                    <input name="rememberme" type="checkbox" class="form-check-input" id="rememberme" value="forever" />
-                    <label class="form-check-label" for="rememberme"><?php _e( 'Remember me', 'woocommerce' ); ?></label>
-                </p>
-
-                <p class="form-row">
-                    <?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
-                    <button type="submit" class="woocommerce-form-login__submit btn btn-outline-primary" name="login" value="<?php esc_attr_e( 'Log in', 'woocommerce' ); ?>"><?php esc_html_e( 'Log in', 'woocommerce' ); ?></button>
-                </p>
-                <p class="woocommerce-LostPassword lost_password mb-0 mt-3">
-                    <a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Lost your password?', 'woocommerce' ); ?></a>
-                </p>
-
-                <?php do_action( 'woocommerce_login_form_end' ); ?>
-
-            </form>
-
-        </div>
-
-        <?php if ( 'yes' === get_option( 'woocommerce_enable_myaccount_registration' ) ) : ?>
-
+        <p>Please <a href="/" class="join-savvy-links" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-user" aria-controls="offcanvas-user">Click Here</a> to register, or log in.</p>
+        
     </div>
 
     <div class="col-md-6">
